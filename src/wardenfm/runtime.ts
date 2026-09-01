8d15855048b9b87f9151f41cf2ab434ae579c4ec
Faiz Ahmed <193629930+Esomoire-consultancy-Company@users.noreply.github.com>
2026-09-02 04:09:31 +0530

    feat: add WardenFM vehicle runtime

---
+++ src/wardenfm/runtime.ts
@@ -0,0 +1,23 @@
+import {
+	WardenFmVehicleSession,
+	type WardenFmCapability,
+	type WardenFmTransport,
+} from './session'
+
+export const WARDENFM_STANDARD_CAPABILITIES: WardenFmCapability[] = [
+	'audio_output',
+	'play_pause',
+	'next_previous',
+	'microphone',
+	'adb_execution',
+]
+
+export const wardenFmVehicleSession = new WardenFmVehicleSession()
+
+export function connectWardenFmVehicle(transport: WardenFmTransport): void {
+	wardenFmVehicleSession.connect(transport, WARDENFM_STANDARD_CAPABILITIES)
+}
+
+export function disconnectWardenFmVehicle(): void {
+	wardenFmVehicleSession.disconnect()
+}
